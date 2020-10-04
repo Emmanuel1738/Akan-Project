c6db38fc3693740cf3e43a8c420ca6b4a4cd6c04
Emmanuel1738 <emmanuelbett13@gmail.com>
2020-10-05 00:35:22 +0300

    added values on variables

---
+++ script.js
@@ -0,0 +1,4 @@
+ 
+var maleNames = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"]
